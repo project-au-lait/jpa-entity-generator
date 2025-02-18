cf0ea829c0891ac34ee224f0bbe6363bded8ebdb
motoki.himeno <motoki.himeno@monocrea.co.jp>
2025-02-18 17:04:47 +0900

    feat: update gen resources

--- jpa-entity-generator-core/src/gen/java/dev/aulait/jeg/core/domain/parentchild/ChildEntity.java
+++ jpa-entity-generator-core/src/gen/java/dev/aulait/jeg/core/domain/parentchild/ChildEntity.java
@@ -4,23 +4,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import javax.annotation.processing.Generated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Generated("dev.aulait.jeg:jpa-entity-generator")
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @Entity
 @Table(name = "child")
 public class ChildEntity extends dev.aulait.jeg.core.domain.BaseEntity
     implements java.io.Serializable {
 
-  @EmbeddedId private ChildEntityId id;
+  @EqualsAndHashCode.Include @EmbeddedId private ChildEntityId id;
 
   @Column(name = "name")
   private String name;
