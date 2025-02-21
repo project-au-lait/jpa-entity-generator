4af0a79e529c59d17cdea0334d3cce2967f78758
motoki.himeno <motoki.himeno@monocrea.co.jp>
2025-02-21 17:12:52 +0900

    chore: add metadata props tableName and columnName

--- jpa-entity-generator-core/src/main/java/dev/aulait/jeg/core/domain/jpa/FieldMetadataModel.java
+++ jpa-entity-generator-core/src/main/java/dev/aulait/jeg/core/domain/jpa/FieldMetadataModel.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class FieldMetadataModel {
   private String fieldName;
+  private String columnName;
   private String javaType;
   private boolean multiple;
   private Integer stringLength;
