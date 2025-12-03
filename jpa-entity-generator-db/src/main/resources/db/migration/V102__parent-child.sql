39a76580f08e9bf9822d5e30d3408ccb566dbc7b
motoki.himeno <motoki.himeno@monocrea.co.jp>
2025-12-03 11:18:28 +0900

    refactor: organize migration script (parent-child)

--- jpa-entity-generator-db/src/main/resources/db/migration/V102__parent-child.sql
+++ jpa-entity-generator-db/src/main/resources/db/migration/V102__parent-child.sql
@@ -12,3 +12,23 @@ CREATE TABLE child (
   --${commonColumns},
   CONSTRAINT child_pk PRIMARY KEY (parent_id, seq_no)
 );
+
+
+CREATE TABLE composite_parent (
+  id_1 CHAR(36),
+  id_2 CHAR(36),
+  name VARCHAR(100),
+  --${commonColumns},
+  CONSTRAINT composite_parent_pk PRIMARY KEY (id_1, id_2)
+);
+
+
+CREATE TABLE composite_child (
+  parent_id_1 CHAR(36),
+  parent_id_2 CHAR(36),
+  seq_no int,
+  name VARCHAR(100),
+  --${commonColumns},
+  CONSTRAINT composite_child_pk PRIMARY KEY (parent_id_1, parent_id_2, seq_no),
+  CONSTRAINT composite_child_parent_id_fkey FOREIGN KEY (parent_id_1, parent_id_2) REFERENCES composite_parent (id_1, id_2)
+);
